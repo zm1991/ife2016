95cbb1d094eb52ae6798e3149265e9a8567a96e6
Weirong Xu <weirongxu.raidou@gmail.com>
2016-03-20 13:43:17 +0800

    添加视网膜屏幕的支持

---
+++ tasks/1-11/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var swig = require('gulp-swig') 
+var cleanCss = require('gulp-clean-css') 
+var autoprefixer = require('gulp-autoprefixer') 
+var browserSync = require('browser-sync')
+
+gulp.task('sass', function() {
+  gulp.src('src/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: [
+        'iOS >= 7',
+        'Android >= 2.0',
+      ],
+    }))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('swig', function() {
+  gulp.src('src/index.html')
+    .pipe(swig())
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist/',
+    },
+    files: ['dist/*.html', 'dist/*.css'],
+  })
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.html', ['swig'])
+  gulp.watch('src/**/*.scss', ['sass'])
+})
+
+gulp.task('build', ['swig', 'sass'])
+gulp.task('dev', ['build', 'watch', 'serve'])
